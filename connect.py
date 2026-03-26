41f414b92e08de7385b3d2dbc0025f56e6344af2
gk2885970-oss <gk2885970@gmail.com>
2026-03-26 14:41:34 +0530

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,12 +1,13 @@
+import os
 from sqlalchemy import create_engine
 
-database_url = "postgresql://espire_database_user:BPoJBnY1X91Q03lHy7fnHseTBLgvpJmA@dpg-d722n2kr85hc73co85p0-a/espire_database"
+database_url = os.environ.get(
+    "DATABASE_URL",
+    "postgresql://espire_database_user:BPoJBnY1X91Q03lHy7fnHseTBLgvpJmA@dpg-d722n2kr85hc73co85p0-a/espire_database"
+)
+
+if database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(database_url, echo=True)
-
-print(f"Connecting to URL {engine.dialect.name}")
-
-
-# with engine.connect() as conn:
-#     conn.execute(text("DROP SCHEMA public CASCADE;"))
-#     conn.execute(text("CREATE SCHEMA public;"))
+print(f"Connecting to: {engine.dialect.name}")
