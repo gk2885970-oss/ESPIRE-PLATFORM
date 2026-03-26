9a84e6dbe7810d79b07335a3a1e00077dfb1e224
gk2885970-oss <gk2885970@gmail.com>
2026-03-26 18:41:28 +0530

    Update connect.py

--- connect.py
+++ connect.py
@@ -3,6 +3,10 @@ from sqlalchemy import create_engine
 
 database_url = os.environ.get("DATABASE_URL")
 
-engine = create_engine(database_url, echo=True)
+engine = create_engine(
+    database_url,
+    echo=True,
+    pool_pre_ping=True  # connection stable rakhega
+)
 
 print(f"Connecting to: {engine.dialect.name}")
